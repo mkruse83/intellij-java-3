3fd349d573887c60af8ec5ec5c2b8b10c94a5392
Martin Kruse <mkruse@prodyna.com>
2021-12-08 00:34:25 +0100

    keep running

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world! With changes.");
+        while (true) {
+            try {
+                Thread.sleep(10000);
+                System.out.println("Tick Java 3");
+            } catch (InterruptedException e) {
+                // silent catch
+            }
+        }
     }
 }
